ba03210d4c3a8901c177d11ddd8c1980a5bf418c
Soders <contact.soders@protonmail.com>
2025-01-04 20:07:39 -0500

    01/04/25  08:07:39 PM

--- Bash-MT1/patch-gmod.sh
+++ Bash-MT1/patch-gmod.sh
@@ -17,57 +17,8 @@ cp /home/soders/Games/GMod-Dark-Theme/GarrysMod/garrysmod/resource/ui/BaseChat.r
 cp /home/soders/Games/GMod-Dark-Theme/GarrysMod/garrysmod/resource/ui/TextWindow.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/TextWindow.res
 cp /home/soders/Games/GMod-Dark-Theme/GarrysMod/garrysmod/resource/fonts/csdbig.ttf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/fonts/
 
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/xenonuke_gateway_station.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/underain_midway.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/thenebeskymuz_city_seventeen.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/teops_g_planet.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/shut_up_bofreemian_rhapsody.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/shikardos007_under_the_bridge.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/sekai_memories.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/ribbit_stools_and_drinks.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/piggo_flight_of_the_construct.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/paz_prop_hunt_alyx.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/paz_3_star_check_in.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/pajtajster_gwick.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/operator7191_gravity.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/olmate_ubafest_shrine_of_the_physgod.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/olmate_ubafest_kowloon_sky_city.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/noshpa_pick_up_that_can.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/noga_the_first_snow.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/noga_meeting_in_the_ruins.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/noga_gm.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/minic23_discover.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/madsonium_welcome_to_gmod.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/lunascribbles_poker_in_space.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/lukaswbrr_one_new_universe.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/lemon_juice_dome_land.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/kushpew_mom_help.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/kushpew_i_want_that_physgun.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/ivretor_super_valve_bros.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/intruso_orangebox_race.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/ink_railroad.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/idiotdoug_red_and_blue.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/idiotdoug_bucket_by_the_window.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/hydralisk_fire.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/hiddenwave8d_noshpa_gmod_workshop.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/hauptmann_cone_thief.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/glassesman_metrocop_in_tunnel.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/gizmo_the_canbine.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/filmchae_making_way.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/edd_construct_isometric.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/du_citadel_core_meltdown.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/dirtypeso_a_world_of_pure_imagination.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/dipski_garrys_express.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/cptsledgehammer_gmod_chapel.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/con_citizens_revolt.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/cobalt_tables_have_turned.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/chernobill_gary_mod.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/candyapple_kleiner_and_lamarr.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/busti_crazy_builders.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/brezz_prop_wars.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/betel_arg_welcome_to_gmod.jpg
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/battleghost_alyx.png
-rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds/alex_gem_our_small_fleet.jpg
+rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
+cp /home/soders/Games/GMod-Dark-Theme/GarrysMod/garrysmod/backgrounds /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
 
 echo -e "${Green}Patched"
 sleep 3
---
+++ Batch/patch-gmod.bat
@@ -0,0 +1,15 @@
+@echo off
+
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\html\template\main.html" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\html\template\main.html"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\materials\console\background01.vtf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\materials\console\background01.vtf"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\materials\console\background01_widescreen.vtf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\materials\console\background01_widescreen.vtf"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\SourceScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\SourceScheme.res"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\ClientScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ClientScheme.res"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\ChatScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ChatScheme.res"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\ui\BaseChat.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ui\BaseChat.res"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\ui\TextWindow.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ui\TextWindow.res"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\resource\fonts\csdbig.ttf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\fonts\"
+XCOPY /y /q "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\gamemodes\Icons\logo.png" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\gamemodes\Icons\logo.png"
+
+del /s /q "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\backgrounds\"
+XCOPY /y /q /s "C:\Users\Soders\Programs\GMod-Dark-Theme\GarrysMod\garrysmod\backgrounds\" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\backgrounds\"
