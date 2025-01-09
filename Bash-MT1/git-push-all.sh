0d69457641929317deaf0c8b00cf6840ae85bbc7
Soders <contact.soders@protonmail.com>
2025-01-09 14:37:09 -0500

    01/09/25  02:37:09 PM

--- Bash-MT1/git-push-all.sh
+++ Bash-MT1/git-push-all.sh
@@ -14,41 +14,57 @@ cp -r /home/soders/Scripts/* /home/soders/Nextcloud/GitHub/Scripts/Scripts/Bash-
 /home/soders/Scripts/git-remove-private.sh
 
 cd /home/soders/Nextcloud/GitHub/Bookmarks/Bookmarks/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/Homepage/Homepage/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/Programs/Programs/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/Scripts/Scripts/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/Configs/Configs/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/Misc/Misc/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
 
 cd /home/soders/Nextcloud/GitHub/BB-Tracker/BB-Tracker/
+git checkout main
+git merge main
 git add .
 git commit -m "$(date +"%D  %I:%M:%S %p")"
 git push origin main
