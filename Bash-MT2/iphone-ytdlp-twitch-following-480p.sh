36f309229fe2a6b28d11fcb95b34aceb9c4849db
Soders <contact.soders@protonmail.com>
2025-01-15 08:37:20 -0500

    01/15/25  08:37:20 AM

--- Bash-MT2/iphone-ytdlp-twitch-following-480p.sh
+++ Bash-MT2/iphone-ytdlp-twitch-following-480p.sh
@@ -18,4 +18,4 @@ echo " " > /home/soders/Configs/YTDLP/watch-history-unsynced.txt
 
 sshpass -p $SSH_PASSWORD scp /home/soders/Configs/YTDLP/watch-history.txt $MT1:/home/soders/Configs/YTDLP/watch-history.txt 2> /dev/null
 
-/home/soders/.local/bin/ytdlp-twitch-following-1080p --match-filter "!is_live" $twitch_bedtime --playlist-end 2 --lazy-playlist --dateafter now-3days --max-downloads 1 > /dev/null 2>&1&
+/home/soders/.local/bin/ytdlp-twitch-following-1080p --match-filter "!is_live" $twitch_following --playlist-end 2 --lazy-playlist --dateafter now-3days --max-downloads 1 > /dev/null 2>&1&
