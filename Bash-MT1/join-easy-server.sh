9dc1dc0d35237d3341c6b1006c8ed00997981e1c
Soders <contact.soders@protonmail.com>
2025-02-06 12:55:06 -0500

    02/06/25  12:55:06 PM

--- Bash-MT1/join-easy-server.sh
+++ Bash-MT1/join-easy-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp /home/soders/Configs/GMod/BB/bb-autoexec.cfg /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/cfg/autoexec.cfg
-steam steam://connect/149.56.38.33:27015
+steam steam://rungameid/4000
+# steam steam://connect/149.56.38.33:27015
 /home/soders/Scripts/disable-bb-tracker.sh
