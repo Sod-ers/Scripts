1aa4087f4c3467682392630dd932208f4fa3e0d8
Soders <contact.soders@protonmail.com>
2025-02-07 16:40:34 -0500

    02/07/25  04:40:34 PM

--- Bash-MT1/git-push-all.sh
+++ Bash-MT1/git-push-all.sh
@@ -45,6 +45,8 @@ cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/cf
 cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/cfg/sprays.cfg /home/soders/Nextcloud/GitHub/Configs/Configs/Open-Fortress/sprays.cfg
 cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/cfg/announcers.cfg /home/soders/Nextcloud/GitHub/Configs/Configs/Open-Fortress/announcers.cfg
 cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/cfg/chat.cfg /home/soders/Nextcloud/GitHub/Configs/Configs/Open-Fortress/chat.cfg
+cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/cfg/practice.cfg /home/soders/Nextcloud/GitHub/Configs/Configs/Open-Fortress/practice.cfg
+cp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/materials/vgui/logos /home/soders/Nextcloud/GitHub/Configs/Configs/Open-Fortress/vgui/logos
 # HL2DM
 cp "/mnt/WIN1/Program Files (x86)/Steam/steamapps/common/Half-Life 2 Deathmatch/hl2mp/cfg/autoexec.cfg" /home/soders/Nextcloud/GitHub/Configs/Configs/HL2DM/autoexec.cfg
 cp "/mnt/WIN1/Program Files (x86)/Steam/steamapps/common/Half-Life 2 Deathmatch/hl2mp/cfg/volume.cfg" /home/soders/Nextcloud/GitHub/Configs/Configs/HL2DM/volume.cfg
--- Bash-MT1/patch-open-fortress.sh
+++ Bash-MT1/patch-open-fortress.sh
@@ -7,6 +7,10 @@ NC='\033[0m'
 
 cp -r /home/soders/Games/Open-Fortress/custom /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/custom
 cp /home/soders/Games/Open-Fortress/lightning_gun.pcf /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/particles/lightning_gun.pcf
+cp /home/soders/Games/Open-Fortress/dm_purple.bsp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/maps/dm_purple.bsp
+cp /home/soders/Games/Open-Fortress/gm_flatgrass_ofd.bsp /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/gm_flatgrass_ofd.bsp
+cp /home/soders/Games/Open-Fortress/hotlinemiami2.vmt /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/materials/vgui/crosshairs/hotlinemiami2.vmt
+cp /home/soders/Games/Open-Fortress/hotlinemiami2.vtf /home/soders/.steam/debian-installation/steamapps/sourcemods/open_fortress/materials/vgui/crosshairs/hotlinemiami2.vtf
 
 echo -e "${GREEN}Patched${NC}"
 sleep 3
