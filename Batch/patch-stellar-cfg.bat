c358db3ba3cf8fd80605113ce00c74a27fcfb42e
Soders <contact.soders@protonmail.com>
2025-02-07 14:39:50 -0500

    02/07/25  02:39:50 PM

--- Batch/patch-stellar-cfg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-del /s /q "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\cfg\autoexec.cfg*"
-
-XCOPY /y /q "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\cfg\stellar-autoexec.cfg" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\cfg\autoexec.cfg*"
