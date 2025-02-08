21cec365a2a521477062a5cd7cec7d09ace95ccd
Soders <contact.soders@protonmail.com>
2025-02-07 20:20:28 -0500

    02/07/25  08:20:28 PM

--- Batch/patch-open-fortress.bat
+++ Batch/patch-open-fortress.bat
@@ -1,4 +1,8 @@
 @echo off
 
-XCOPY /y /q "E:\Games\Open-Fortress\custom" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\custom\"
+XCOPY /y /q /s "E:\Games\Open-Fortress\custom" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\custom\"
 XCOPY /y /q "E:\Games\Open-Fortress\lightning_gun.pcf" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\particles\lightning_gun.pcf*"
+XCOPY /y /q "E:\Games\Open-Fortress\hotlinemiami2.vtf" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\materials\vgui\crosshairs\hotlinemiami2.vtf*"
+XCOPY /y /q "E:\Games\Open-Fortress\hotlinemiami2.vmt" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\materials\vgui\crosshairs\hotlinemiami2.vmt*"
+XCOPY /y /q "E:\Games\Open-Fortress\dm_purple.bsp" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\maps\dm_purple.bsp*"
+XCOPY /y /q "E:\Games\Open-Fortress\gm_flatgrass_ofd.bsp" "C:\program files (x86)\steam\steamapps\sourcemods\open_fortress\maps\gm_flatgrass_ofd.bsp*"
