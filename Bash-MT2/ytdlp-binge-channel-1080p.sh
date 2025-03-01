c68b64824362a1f27424c68126045b7dd75f861a
Soders <contact.soders@protonmail.com>
2025-02-28 21:58:08 -0500

    02/28/25  09:58:08 PM

--- Bash-MT2/ytdlp-binge-channel-1080p.sh
+++ Bash-MT2/ytdlp-binge-channel-1080p.sh
@@ -20,4 +20,4 @@ cp ~/Configs/YTDLP/watch-history.txt ~/Configs/YTDLP/Backup/watch-history.txt
 
 ~/Scripts/delete-empty-media-directories.sh && ~/Scripts/delete-empty-media-directories-2.sh
 
-~/.local/bin/ytdlp --match-filter "!was_live & original_url!*=/shorts/" $binge_channel_url --config-locations ~/Configs/YTDLP/youtube-video-1080p.conf --lazy-playlist --max-downloads 10 --download-archive ~/Configs/YTDLP/watch-history.txt > /dev/null 2>&1&
+~/.local/bin/ytdlp --match-filter '!was_live & original_url!*=/shorts/' $binge_channel_url --config-locations ~/Configs/YTDLP/youtube-video-1080p.conf --lazy-playlist --max-downloads 10 --download-archive ~/Configs/YTDLP/watch-history.txt > /dev/null 2>&1&
