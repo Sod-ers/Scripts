323b12489471cc023a2108910a246cececc525a6
Soders <contact.soders@protonmail.com>
2025-06-02 11:00:24 -0400

    06/02/25  11:00:24 AM

---
+++ Batch/disable-gamebar-controller-popups.bat
@@ -0,0 +1,10 @@
+## OPEN POWERSHELL AS ADMIN & RUN A COMMAND LIKE SO: cmd.exe -c "E:\Scripts\disable-gamebar-controller-popups.bat"
+
+reg add HKCR\ms-gamebar /f /ve /d URL:ms-gamebar 2>&1 >''
+reg add HKCR\ms-gamebar /f /v "URL Protocol" /d "" 2>&1 >''
+reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d "" 2>&1 >''
+reg add HKCR\ms-gamebar\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >''
+reg add HKCR\ms-gamebarservices /f /ve /d URL:ms-gamebarservices 2>&1 >''
+reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d "" 2>&1 >''
+reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d "" 2>&1 >''
+reg add HKCR\ms-gamebarservices\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >'' 
