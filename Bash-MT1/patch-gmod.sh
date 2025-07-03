f0153bb3597b97b0b5d8406d875ab7beec145216
Soders <contact.soders@protonmail.com>
2025-07-03 08:29:45 -0400

    07/03/25  08:29:45 AM

--- Bash-MT1/patch-gmod.sh
+++ Bash-MT1/patch-gmod.sh
@@ -13,17 +13,10 @@ do
     case $opt in
         "Full Patch")
 ~/.local/bin/gmodpatchtool
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/html/template/main.html /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/html/template/main.html
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/SourceScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/SourceScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ClientScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ClientScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ChatScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ChatScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/BaseChat.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/BaseChat.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/TextWindow.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/TextWindow.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/fonts/csdbig.ttf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/fonts/
+cp -r /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/garrysmod/addons/dark-theme /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/addons
 rm -r /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
 cp -r /home/soders/Games/GMod/backgrounds /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
+cp /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/cfg/bb-autoexec.cfg /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/cfg/autoexec.cfg
 echo -e "${GREEN}Patched${NC}"
 sleep 3
             break
@@ -35,15 +28,7 @@ sleep 3
             break
             ;;
         "Custom Files Only")
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/html/template/main.html /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/html/template/main.html
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/SourceScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/SourceScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ClientScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ClientScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ChatScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ChatScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/BaseChat.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/BaseChat.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/TextWindow.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/TextWindow.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/fonts/csdbig.ttf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/fonts/
+cp -r /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/garrysmod/addons/dark-theme /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/addons
 rm -r /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
 cp -r /home/soders/Games/GMod/backgrounds /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
 cp /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/cfg/bb-autoexec.cfg /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/cfg/autoexec.cfg
@@ -52,17 +37,7 @@ sleep 3
             break
             ;;
         "Default Menu")
-cp /home/soders/Games/GMod/default-menu.html /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/html/template/main.html
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/materials/console/background01_widescreen.vtf
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/SourceScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/SourceScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ClientScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ClientScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ChatScheme.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ChatScheme.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/BaseChat.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/BaseChat.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/ui/TextWindow.res /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/ui/TextWindow.res
-cp /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/resource/fonts/csdbig.ttf /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/resource/fonts/
-rm -r /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
-cp -r /home/soders/Nextcloud/GitHub/GMod-Resources/GMod-Resources/Dark-Theme/Linux/GarrysMod/garrysmod/backgrounds /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/backgrounds
+rm /home/soders/.steam/debian-installation/steamapps/common/GarrysMod/garrysmod/addons/dark-theme/html/template/main.html
 echo -e "${GREEN}Patched${NC}"
 sleep 3
             break
--- Batch/patch-gmod-regular.bat
+++ Batch/patch-gmod-regular.bat
@@ -1,12 +1,3 @@
 @echo off
 
-XCOPY /y /q "E:\Games\GMod\regular-menu.html" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\html\template\main.html*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\materials\console\background01.vtf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\materials\console\background01.vtf*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\materials\console\background01_widescreen.vtf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\materials\console\background01_widescreen.vtf*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\SourceScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\SourceScheme.res*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\ClientScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ClientScheme.res*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\ChatScheme.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ChatScheme.res*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\ui\BaseChat.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ui\BaseChat.res*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\ui\TextWindow.res" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\ui\TextWindow.res*"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\resource\fonts\csdbig.ttf" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\resource\fonts\"
-XCOPY /y /q "E:\GitHub\GMod-Resources\Dark-Theme\Windows\GarrysMod\garrysmod\gamemodes\Icons\logo.png" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\gamemodes\Icons\logo.png*"
+del /s /q "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\dark-theme\html\template\main.html*"
